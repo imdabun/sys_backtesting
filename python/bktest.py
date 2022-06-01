8f1f716f88e3297962e57604a9ca7d1d7502f1ff
Gan Yang <yanggan1998@gmail.com>
2022-06-01 19:53:47 -0400

    files tbd added, backtest class written

--- python/bktest.py
+++ python/bktest.py
@@ -5,6 +5,7 @@ CMU Algorithmic Trading Group (ATG) Systems Team
 """
 
 import numpy as np
+import pandas as pd
 
 class Universe:
     """
@@ -17,21 +18,39 @@ class Universe:
     s     Swaps
     """
 
+    def info ( self ):
+        """
+        Displays a description of the universe
+        """
+        raise NotImplementedError
+
     def forward( self, t_idx: int, alpha: np.ndarray)-> np.ndarray:
         """
         Takes in time index and alpha array, returns the PnL calculation from last t_idx to current
         """
         raise NotImplementedError
 
-    def wmp_status( self)-> bool:
+    def burnin_status( self)-> bool:
         """
-        Returns whether the warmup period is complete and testing phase can begin
+        Returns whether the burn-in period is complete and testing phase can begin
         """
         raise NotImplementedError
 
 
 class StratScheduler:
     """Strategy Scheduler class that schedules alpha update timings for one strategy"""
+
+    def info ( self ):
+        """
+        Displays a description of the scheduler
+        """
+        raise NotImplementedError
+
+    def univ( self, universe: Universe ):
+        """
+        Method to select the universe for the scheduler
+        """
+        self.universe = universe
 
     def update( self)-> int:
         """
@@ -45,19 +64,26 @@ class TradeScheduler:
 
     def __init__( self, strat_schedulers: dict):
         """
-        Takes in a dictionary of strategy schedulers
+        Takes in a dictionary of strategy schedulers with the same universe
         """
         self.strat_schedulers = strat_schedulers
+        self.universe = None
 
-    def update( self)-> str, int:
+    def univ( self, universe: Universe ):
         """
-        A generator that yields the tuple (strategy, t_idx) as it is called
-        >> Have to think about how to implement it most efficiently 
-        >> What if we want to update a subset of strategies at the same time 
+        Method to select the universe for the scheduler
+        """
+        for strat in self.strat_schedulers:
+            strat.univ(universe)
+
+    def update( self )-> '(str, int)':
+        """
+        A generator that yields the tuple (strat_dict, t_idx) as it is called
+        Here strat_dict is a dictionary of all strategies to be updated
         """
         raise NotImplementedError
 
-    def update_all( self)-> int:
+    def update_all( self )-> 'int generator':
         """
         A generator that yields t_idx to update all strategies
         """
@@ -66,6 +92,18 @@ class TradeScheduler:
 
 class Strategy:
     """Alpha class that describes alpha signal/trading logic"""
+
+    def info ( self ):
+        """
+        Displays a description of the strategy
+        """
+        raise NotImplementedError
+
+    def univ( self, universe: Universe ):
+        """
+        Method to select the universe for the strategy
+        """
+        self.universe = universe
 
     def forward( self, t_idx: int)-> np.ndarray:
         """
@@ -85,13 +123,21 @@ class StratLib:
         self.lib = {}
         self.alphas = {}
 
-    def forward( self, strat: str, t_idx: int)-> None:
+    def univ( self, universe: Universe ):
+        """
+        Method to select the universe for all strategies
+        """
+        for strat in self.lib:
+            self.lib[strat].univ(universe)
+
+    def forward( self, strats: dict, t_idx: int)-> None:
         """
         The trade function called on a specific strat
         >> Have to think about how to implement it most efficiently 
         >> What if we want to update a subset of strategies at the same time 
         """
-        self.alphas[strat] = self.lib[strat].forward(t_idx)
+        for strat in strats:
+            self.alphas[strat] = self.lib[strat].forward(t_idx)
 
     def forward_all( self, t_idx: int)-> None:
         """
@@ -105,6 +151,12 @@ class PortfolioOpt:
     """Portfolio Optimizer class that combines and optimizes a portfolio based
     on alpha signals in the strat library"""
 
+    def info ( self ):
+        """
+        Displays a description of the portfolio optimizer
+        """
+        raise NotImplementedError
+
     def forward( self, t_idx: int, strat_lib: StratLib) -> np.ndarray:
         """
         Function that calculates values to assign to instruments based on
@@ -115,20 +167,56 @@ class PortfolioOpt:
 
 class Backtest:
     """Backtest class that runs a single backtest using given parameters and strat lib"""
+    OUT_LABELS = ["t_idx", "long_pnl", "short_pnl", "long_inst", "short_inst"]
 
-    def __init__( self, btsettings: dict, # start_time, test_time, end_time, t_unit
+    def __init__( self, btsettings: dict, # universe, start_time, burn_in, end_time
                         trade_scheduler: TradeScheduler,
                         strat_lib: StratLib,
-                        portfolio_opt: PortfolioOpt,):
+                        portfolio_opt: PortfolioOpt ):
         self.btsettings = btsettings
         self.trade_scheduler = trade_scheduler
         self.strat_lib = strat_lib
         self.portfolio_opt = portfolio_opt
+        self.output = []
+
+        self.universe = None
+        # create universe
+        if self.btsettings['univ'] == 'custom':
+            self.universe = custom_univ( self.btsettings['start_time'],
+                                    self.btsettings['burn_in'],
+                                    self.btsettings['end_time'])
+        else:
+            self.universe = create_univ( self.btsettings['start_time'],
+                                    self.btsettings['burn_in'],
+                                    self.btsettings['end_time'])
+        self.trade_scheduler.univ(self.universe)
+        self.strat_lib.univ(self.universe)
+        self.portfolio_opt.univ(self.universe)
 
     def run(self):
-      
-        pass # TODO
+        """
+        Main function to run the backtest
+        """
+        if self.btsettings['sync_schedule']:
+            # synchronized scheduler updates for all
+            for t_idx in self.trade_scheduler.update_all():
+                self.strat_lib.forward_all(t_idx)
+                alpha_vec = self.portfolio_opt.forward(t_idx, self.strat_lib)
+                out_vec = self.universe.forward(t_idx, alpha_vec)
+                if not self.universe.burnin_status():
+                    self.output.append(out_vec)
+        else:
+            # desynchronized scheduling; i.e. at each t_idx only a subset updated
+            for strats, t_idx in self.trade_scheduler.update():
+                self.strat_lib.forward(strats, t_idx)
+                alpha_vec = self.portfolio_opt.forward(t_idx, self.strat_lib)
+                out_vec = self.universe.forward(t_idx, alpha_vec)
+                if not self.universe.burnin_status():
+                    self.output.append(out_vec)
 
-    def dump(self):
-
-        pass # TODO
+    def dump(self, **kwargs):
+        """
+        Dump the output dataframe saved after running the backtest
+        """
+        data_frame = pd.DataFrame(data=self.output, columns=self.OUT_LABELS)
+        data_frame.to_csv(**kwargs)
